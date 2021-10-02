33f4f2170172188eb6f631ef3bfd243125c9a5c4
Alex <math.sqrta@gmail.com>
2021-10-02 17:00:00 +0200

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Container>
+        <Row>
+          <Col>
+            <NavBar />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,15 +4,17 @@ function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <img className="navbar-logo" src={navimg}></img>
-        <div className="navbar-cityinfo">
-          Доставка пиццы <span>Москва</span>
-          <br /> 35 мин - 4.67 h{' '}
+        <div className="navbar-left-main">
+          <img className="navbar-left-logo" src={navimg}></img>
+          <div className="navbar-left-cityinfo">
+            Доставка пиццы <span>Москва</span>
+            <br /> 35 мин - 4.67 h{' '}
+          </div>
+          <div className="navbar-left-phone">
+            <a href="tel:+89991119911">8 999 111-99-11</a>
+            <span>Звонок бесплатный</span>
+          </div>
         </div>
-      </div>
-      <div className="navbar-middle">
-        <a href="tel:+89991119911">8 999 111-99-11</a>
-        <span>Звонок бесплатный</span>
       </div>
       <div className="navbar-right">Додокоины Мои акции Алексей</div>
     </div>
